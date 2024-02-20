fc67066f78db53d4476b5491f9ef2bc35eb8ca54
Joel Caban <joel.caban2017@gmail.com>
2024-02-20 14:27:35 -0400

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  *main- Entry Point
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char letter ;
+	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	putchar(letter);
 	putchar('\n');
