18fc4753f5e0587f0f46014af90ffc19a6dfde0a
Joel Caban <joel.caban2017@gmail.com>
2024-02-20 22:48:38 -0400

    8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 48; num++)
-	if (num < 10)
-	putchar(num + '0');
-	putchar(num - 10 + 'a');
+	for (num = 0; num < 10; num++)
+	putchar('0' + num);
+	for (num = 0; num < 6; num++)
+	putchar('a' + num);
 	putchar('\n');
 	return (0);
 }
