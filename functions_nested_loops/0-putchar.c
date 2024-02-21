d85dc73baf4579f548a5ba49386f248c2bd439b6
Joel Caban <joel.caban2017@gmail.com>
2024-02-21 10:38:33 -0400

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ *main- Prints _putchar, followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *str = "_putchar";
+
+	for (int 1 = 0; str[i] != '\0'; i++)
+	{
+	putchar(str[i]);
+	}
+	putchar('\n');
+	return (0);
+}
