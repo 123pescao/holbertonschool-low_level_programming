11feaa45438dba3912c3f0721d825fd07ffaeaca
Joel Caban <joel.caban2017@gmail.com>
2024-02-22 13:21:42 -0400

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line- Draws a straight line in terminal.
+ */
+void print_line(int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
