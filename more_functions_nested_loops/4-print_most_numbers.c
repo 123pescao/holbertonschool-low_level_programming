2ba7985e41e028c12950736aaea1b9ae1d67bfc4
Joel Caban <joel.caban2017@gmail.com>
2024-02-22 11:57:09 -0400

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,11 @@ void print_most_numbers(void)
 	char letter;
 
 	for (letter = '0'; letter <= '9'; letter++)
-	if (letter != '2' || letter != '4')
+	{
+		if (letter != '2' || letter != '4')
 	{
 		_putchar(letter);
 	}
 		_putchar('\n');
+	}
 }
