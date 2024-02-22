a3cef30afe9ad9862bf59b598753db06c0635273
Joel Caban <joel.caban2017@gmail.com>
2024-02-22 11:52:32 -0400

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers- prints the numbers
+ */
+void print_most_numbers(void)
+{
+	char letter;
+
+	for (letter = '0'; letter <= '9'; letter++)
+	if (letter != '2' || letter != '4')
+	{
+		_putchar(letter);
+		_putchar('\n');
+	}
+}
