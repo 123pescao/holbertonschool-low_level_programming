e8a6bcdc1545fdf612b558813144ce3c1ce7b584
Joel Caban <joel.caban2017@gmail.com>
2024-02-21 22:00:24 -0400

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -7,12 +7,12 @@
  */
 int _abs(int n)
 {
-	if (n > 0)
+	if (n < 0)
 	{
-		int abs_value;
-		
-		abs_value = n * -1;
-		return (abs_value);
+	int abs_value;
+	
+	abs_value = n * -1;
+	return (abs_value);
 	}
-		return (n);
+	return (n);
 }
