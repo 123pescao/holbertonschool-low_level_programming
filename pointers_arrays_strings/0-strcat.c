4b1abc6bfd993b51d284c88f8c1f69dfd2fa6f53
Joel Caban <joel.caban2017@gmail.com>
2024-02-28 10:07:00 -0400

    0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat- concatenates two strings
+ * @dest: destination
+ * @src: src
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a, b = 0;
+
+	while (dest[b])
+	{
+		b++;
+	}
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[b] = src[a];
+		b++;
+	}
+	dest[b] = '\0';
+	return (dest);
+}
