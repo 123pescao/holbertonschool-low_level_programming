5b3b6df4bd1e7a105654db670f8785cec5676bb4
Joel Caban <joel.caban2017@gmail.com>
2024-02-28 11:16:29 -0400

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- *_strncpy- copies a string
+ *_strncpy- copy two strings
  *@dest: destination
  *@src: src
+ *@n: bytes
  *Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int a = 0, b = 0;
 
-	for (a = 0; src[a]; && a < n; a++)
+	for (a = 0; src[a] && a < n; a++)
 	{
 		dest[a] = src[a];
 		b++;
