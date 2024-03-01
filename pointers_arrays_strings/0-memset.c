ff3305ceb81e36c186967bda8611f8affeb073d0
Joel Caban <joel.caban2017@gmail.com>
2024-02-29 20:25:41 -0400

    0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *_memset- fills memory with a constant byte
+ @s: source string
+ @b: the constant byte for filling
+ @n: length of buffer
+ * Return: New string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
+}
+
