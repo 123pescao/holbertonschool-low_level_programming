7e597f47236cffab7a59a28a80732499712f86cf
Joel Caban <joel.caban2017@gmail.com>
2024-03-05 20:55:00 -0400

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main- Prints its name
- * @argc: argument count
- * @argv: argument vector
+ *main- Prints its name
+ *@argc: argument count
+ *@argv: argument vector
  *
  * Return: Always 0
  */
@@ -11,6 +11,5 @@ int main(int argc, char *argv[])
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
