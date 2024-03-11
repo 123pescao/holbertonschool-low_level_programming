dedaba4930d0a84f3eadc6f57d8679f19b37d7c7
Joel Caban <joel.caban2017@gmail.com>
2024-03-11 15:07:28 -0400

    1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog- initializes the variable
+ * @d: variable
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
