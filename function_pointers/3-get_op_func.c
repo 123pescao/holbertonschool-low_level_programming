19a9a6e455c52dcd288bd5c223eb9da0b087f642
Joel Caban <joel.caban2017@gmail.com>
2024-03-13 21:13:28 -0400

    3get

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,9 +1,10 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - selects the correct function to perform the operation
- * @s: operator passed as argument
- * Return: pointer to the function that corresponds to the operator
+ * get_op_func - gets operator and redirects to func
+ * @s: operator
+ *
+ * Return: return null or function
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -15,14 +16,16 @@ int (*get_op_func(char *s))(int, int)
         {"%", op_mod},
         {NULL, NULL}
     };
-    int i = 0;
-
-    while (ops[i].op != NULL)
+    int i;
+	
+	i = 0;
+	while (ops[i].op != NULL)
     {
-        if (*s == *ops[i].op && s[1] == '\0')
-            return (ops[i].f);
+        if (strcmp(s, ops[i].op) == 0)
+		{
+            return (*(ops[i]).f);
+		}
         i++;
     }
-
-    return (NULL);
+	return (NULL);
 }
